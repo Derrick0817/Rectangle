6e4521435de318b66b2849f210446711bedd435a
Derrick0817 <derrickorz@gmail.com>
2024-04-01 07:54:36 -0400

    Add new features

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,41 +24,98 @@ namespace WpfApp1
             InitializeComponent();
             heightInput.TextChanged += heightInput_TextChanged;
             areaInput.TextChanged += areaInput_TextChanged;
+            menu.SelectionChanged += menu_SelectionChanged;
         }
 
-        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        public void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             // Only allow numeric input
             e.Handled = !double.TryParse(e.Text, out _);
         }
 
-        public void heightInput_TextChanged(object sender, TextChangedEventArgs e)
+        public void heightInput_TextChanged(object sender, TextChangedEventArgs e) //calculate width and height base on shape and text entries when height is changed
         {
             if (double.TryParse(heightInput.Text, out double height) && height > 0)
             {
-                double currentArea = double.Parse(areaInput.Text); //convert input to double
-                double width = currentArea / (double)height;
-                UpdateRectangleSize(width, (double)height);
+                string selectedShapeType = (menu.SelectedItem as ComboBoxItem)?.Content.ToString();
+                double currentArea = 0;
+                double width = 0;
+                switch (selectedShapeType)
+                {
+                    case "Rectangle":
+                        currentArea = double.Parse(areaInput.Text); //  try convert input to double
+                        width = currentArea / height;
+                        break;
+                    case "Ellipse":
+                        currentArea = double.Parse(areaInput.Text); //  try convert input to double
+                        width = currentArea / height / Math.PI;
+                        break;
+                }
+                UpdateSize(width, (double)height);
             } 
         }
 
-        private void areaInput_TextChanged(object sender, TextChangedEventArgs e)
+        public void areaInput_TextChanged(object sender, TextChangedEventArgs e) //calculate width and height base on shape and text entries when area is changed
         {
             if (double.TryParse(areaInput.Text, out double area) && area > 0)
             {
-                double currentHeight = double.Parse(heightInput.Text); //  try convert input to double
-                double width = (double)area / currentHeight;
-                UpdateRectangleSize(width, currentHeight);
+                string selectedShapeType = (menu.SelectedItem as ComboBoxItem)?.Content.ToString();
+                double currentHeight = 0;
+                double width = 0;
+                switch (selectedShapeType)
+                {
+                    case "Rectangle":
+                        currentHeight = double.Parse(heightInput.Text); //  try convert input to double
+                        width = (double)area / currentHeight;
+                        break;
+                    case "Ellipse":
+                        currentHeight = double.Parse(heightInput.Text); //  try convert input to double
+                        width = (double)area / currentHeight /Math.PI;
+                        break;
+                }
+                UpdateSize(width, currentHeight);
             }
         }
 
-        public void UpdateRectangleSize(double width, double height)
+
+        public void menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // Check if the new size exceeds the window's dimensions and update rectangle size
+            UpdateShape();
+        }
+
+        public void UpdateShape() // change shape by changing visibility
+        {
+            string selectedShapeType = (menu.SelectedItem as ComboBoxItem)?.Content.ToString();
+            switch (selectedShapeType)
+            {
+                case "Rectangle":
+                    rectangle.Visibility = Visibility.Visible;
+                    ellipse.Visibility = Visibility.Hidden;
+                    break;
+                case "Ellipse":
+                    rectangle.Visibility = Visibility.Hidden;
+                    ellipse.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        
+        public void UpdateSize(double width, double height) // update size base on current selected shape
+        {
             if (width > 0 && width <= this.Width && height > 0 && height <= this.Height)
             {
-                rectangle.Width = width;
-                rectangle.Height = height;
+                string selectedShapeType = (menu.SelectedItem as ComboBoxItem)?.Content.ToString();
+                switch (selectedShapeType)
+                {
+                    case "Rectangle":
+                        rectangle.Width = width;
+                        rectangle.Height = height;
+                        break;
+                    case "Ellipse":
+                        ellipse.Width = width;
+                        ellipse.Height = height;
+                        break;
+                }
             }
         }
 
